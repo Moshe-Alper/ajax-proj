effe70fe5d5b5a87d9f5099180e3ef30da3aa127
Moshik Alper <moshikalper@gmail.com>
2024-08-19 20:42:42 +0300

    Add date with XMLHttpRequest

--- js/controllers/address-book.controller.js
+++ js/controllers/address-book.controller.js
@@ -1,1 +1,36 @@
 'use strict'
+
+function onInit() {
+    getList(renderList)
+
+}
+
+function renderList(data) {
+
+    const elAddressSection = document.querySelector('.address-section')
+    const strHTML = data.map(address => `
+ <article class="address-book-item">
+            <h3 class="info-name">${address.lname}, ${address.fname}</h3>
+            <img src="https://robohash.org/${address.fname} ${address.lname}" alt="User Image">
+            <ul class="info">
+                <li>
+                    Phone: <span class="info-phone">${address.tel}</span>
+                </li>
+                <li>
+                    City: <span class="info-city">${address.city}</span>
+                </li>
+                <li>
+                    State: <span class="info-state">${address.state}</span>
+                </li>
+                <li>
+                    Zip: <span class="info-zip">${address.zip}</span>
+                </li>
+                <li>
+                    Address: <span class="info-address">${address.address}</span>
+                </li>
+            </ul>
+        </article>
+        `)
+        
+        elAddressSection.innerHTML = strHTML.join('')
+}
--- js/services/address-book.service.js
+++ js/services/address-book.service.js
@@ -1,1 +1,13 @@
 'use strict'
+
+function getList(cb) {
+    const xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            const list = JSON.parse(xhr.responseText)
+            cb(list)
+        }
+    }
+    xhr.open('GET', 'lib/FillTextAPI.json', true)
+    xhr.send()
+}
